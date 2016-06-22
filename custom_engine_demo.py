915eff09a2037dd149f94bdbdad7de0f5a8c7643
ezhguox <ezhguox@CN00103287.ericsson.se>
2016-06-22 16:26:38 +0800

    u

--- custom_engine_demo.py
+++ custom_engine_demo.py
@@ -1,6 +1,9 @@
 
 from custom import fixedmainengine
+from multiprocessing import freeze_support
 
-m = fixedmainengine.FixedMainEngine(broker='yh', need_data='yh.json', ext_stocks=['159915'])
-m.load_strategy()
-m.start()
+if __name__ == '__main__':
+    freeze_support()
+    m = fixedmainengine.FixedMainEngine(broker='yh', need_data='yh.json', ext_stocks=['159915'])
+    m.load_strategy()
+    m.start()
