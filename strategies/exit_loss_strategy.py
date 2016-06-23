4e75b69ac1a45ad3df270dfeb9c2d4ab5b8d669b
ezhguox <ezhguox@CN00103287.ericsson.se>
2016-06-23 13:34:47 +0800

    u

--- strategies/exit_loss_strategy.py
+++ strategies/exit_loss_strategy.py
@@ -1,33 +1,33 @@
 from easyquant import StrategyTemplate
 import easyhistory
 
-def get_exit_price(hold_codes=['300162']):#, has_update_history=False):
-    #exit_dict={'300162': {'exit_half':22.5, 'exit_all': 19.0},'002696': {'exit_half':17.10, 'exit_all': 15.60}}
-    has_update_history = True
-    if not has_update_history:
-        easyhistory.init('D', export='csv', path="C:/hist",stock_codes=hold_codes)
-        easyhistory.update(path="C:/hist",stock_codes=hold_codes)
-        #has_update_history = True
-    his = easyhistory.History(dtype='D', path='C:/hist',codes=hold_codes)
-    exit_dict = dict()
-    for code in hold_codes:
-        #code_hist_df = hist[code].MA(1).tail(3).describe()
-        exit_data = dict()
-        describe_df = his[code].MA(1).tail(3).describe()
-        min_low =round(describe_df.loc['min'].low, 2)
-        min_close = round(round(describe_df.loc['min'].close,2),2)
-        max_close = round(describe_df.loc['max'].close,2)
-        max_high = round(describe_df.loc['max'].high,2)
-        exit_data['exit_half'] = min_close
-        exit_data['exit_all'] = min_low
-        exit_dict[code] = exit_data
-    #print('exit_dict=%s' % exit_dict)
-    return exit_dict
 
 class Strategy(StrategyTemplate):
     name = '止损策略'
     #exit_data ={}
     #has_update_history = False
+    def get_exit_price(self, hold_codes=['300162']):#, has_update_history=False):
+        #exit_dict={'300162': {'exit_half':22.5, 'exit_all': 19.0},'002696': {'exit_half':17.10, 'exit_all': 15.60}}
+        has_update_history = True
+        if not has_update_history:
+            easyhistory.init('D', export='csv', path="C:/hist",stock_codes=hold_codes)
+            easyhistory.update(path="C:/hist",stock_codes=hold_codes)
+            #has_update_history = True
+        his = easyhistory.History(dtype='D', path='C:/hist',codes=hold_codes)
+        exit_dict = dict()
+        for code in hold_codes:
+            #code_hist_df = hist[code].MA(1).tail(3).describe()
+            exit_data = dict()
+            describe_df = his[code].MA(1).tail(3).describe()
+            min_low =round(describe_df.loc['min'].low, 2)
+            min_close = round(round(describe_df.loc['min'].close,2),2)
+            max_close = round(describe_df.loc['max'].close,2)
+            max_high = round(describe_df.loc['max'].high,2)
+            exit_data['exit_half'] = min_close
+            exit_data['exit_all'] = min_low
+            exit_dict[code] = exit_data
+        #print('exit_dict=%s' % exit_dict)
+    return exit_dict
 
     def strategy(self, event):
         self.log.info('\n\n止损策略执行中。。。')
@@ -41,7 +41,7 @@ class Strategy(StrategyTemplate):
         #trade_code = list(set(holding_stock).difference(set(except_code_list)))
         trade_code = self.trade_stocks
         self.log.info('股票止损监测：  %s'  % trade_code)
-        exit_data = get_exit_price(trade_code)
+        exit_data = self.get_exit_price(trade_code)
         self.log.info('止损点：  %s'  % exit_data)
         for event_code in trade_code:
             if event_code in list(event.data.keys()):
